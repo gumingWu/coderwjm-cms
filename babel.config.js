024cfa219a7218e3b4eb0c6c6f34438b4a52d4d6
gumingWu <15626185054@163.com>
2021-10-17 16:48:37 +0800

    build(babel): 使用babel-plugin-dynamic-import-node插件，区分环境加载组件

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
+const plugins = []
+
+if (process.env.NODE_ENV === 'development') {
+  console.log(1)
+  plugins.push('dynamic-import-node')
+} else {
+  console.log(2)
+}
+
 module.exports = {
-  presets: ['@vue/cli-plugin-babel/preset']
+  presets: ['@vue/cli-plugin-babel/preset'],
+  plugins
 }
