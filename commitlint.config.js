451e2c6fa8de2b63bd785b8cf11d1b34733b7f4a
gumingWu <15626185054@163.com>
2021-11-21 15:13:58 +0800

    build(commitlint): 完善commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,6 +5,7 @@ module.exports = {
   rules: {
     'type-empty': [2, 'never'],
     'type-enum': [2, 'always', types],
+    'scope-empty': [2, 'never'],
     'subject-empty': [2, 'never'],
     'header-max-length': [2, 'always', 50]
   }
