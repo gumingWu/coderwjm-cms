7fb0aaa48405ae1add6a112f58681e30c45aa3c0
gumingWu <15626185054@163.com>
2021-11-21 15:01:30 +0800

    build: 初始化cli，能运行

---
+++ cli/src/bin.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import { Command } from 'commander'
+import { onCreate } from './commands/create'
+
+const program = new Command()
+
+program
+  .command('create [name...]')
+  .option('-t --type <type>', 'select create type')
+  .description('create a views, or a route, or a pinia module')
+  .action(onCreate)
+
+program.parse(process.argv)
